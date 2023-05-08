bc20b5dedb191fae8c935458e8e5e67bf0f00951
gework7 <s1159783@student.windesheim.nl>
2023-05-08 13:26:25 +0200

    Add marks on board and check winner

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
+    ['X', 'O', 'O'],
+    ['X', 'O', ''],
+    ['X', '', '']
 ]
 
 let winner = null;
 let ai = 'X';
 let currentPlayer = human = 'O';
-let cellWidth, cellHeight, cross, circle;
+let cellWidth, cellHeight, crossMark, circleMark;
 
 function preload() {
-    cross = loadImage('assets/cross.png');
-    circle = loadImage('assets/circle.png');
+    crossMark = loadImage('assets/cross.png');
+    circleMark = loadImage('assets/circle.png');
 }
 
 function setup() {
@@ -21,18 +21,47 @@ function setup() {
     cellHeight = height / 3;
 }
 
-function draw() {
+function drawGrid() {
     line(0, cellHeight, cellWidth * 3, cellHeight);
     line(0, cellHeight * 2, cellWidth * 3, cellHeight * 2);
-
     line(cellWidth, 0, cellWidth, cellHeight * 3);
     line(cellWidth * 2, 0, cellWidth * 2, cellHeight * 3);
+}
+
+function draw() {
+    strokeWeight(4);
+    drawGrid();
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+
+            let x = cellHeight * j + cellHeight / 2;
+            let y = cellWidth * i + cellWidth / 2;
+            let spot = board[i][j];
+
+            if (spot === human) {
+                image(circleMark, x - 60, y - 60, 120, 120);
+            } else if (spot === ai) {
+                image(crossMark, x - 60, y - 60, 120, 120);
+            }
+        }
+    }
+
+    let result = checkWinner();
+    if (result !== null) {
+        noLoop();
+        if (result === 'tie') {
+            document.getElementById('result').innerHTML = 'Tie!';
+        } else {
+            document.getElementById('result').innerHTML = `${result} wins`;
+        }
+    }
 }
 
 function countOpenSpots() {
     let count = 0;
     for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
             if (board[i][j] === '') {
                 count++;
             }
@@ -68,13 +97,15 @@ function checkWinner() {
     if (equals3(board[2][0], board[1][1], board[0][2])) {
         return board[2][0];
     }
+
+    return countOpenSpots() === 0 ? 'tie' : null;
 }
 
 function makeMove() {
     let x = floor(mouseX / cellWidth);
     let y = floor(mouseY / cellHeight);
 
-    if (board[x][y] == '') {
+    if (checkWinner() == null && board[x][y] == '') {
         board[x][y] = currentPlayer;
         currentPlayer = ai;
     }
