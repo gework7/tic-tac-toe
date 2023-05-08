0fcf61b0a81ef3781655ff7ab106457451b28a2c
gework7 <s1159783@student.windesheim.nl>
2023-05-08 12:09:44 +0200

    Add makeMove function

---
+++ app.js
@@ -0,0 +1,40 @@
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+
+let winner = null;
+let ai = 'X';
+let currentPlayer = human = 'O';
+let cellWidth, cellHeight, cross, circle;
+
+function preload() {
+    cross = loadImage('assets/cross.png');
+    circle = loadImage('assets/circle.png');
+}
+
+function setup() {
+    let canvas = createCanvas(450, 450);
+    canvas.mousePressed(makeMove);
+    cellWidth = width / 3;
+    cellHeight = height / 3;
+}
+
+function draw() {
+    line(0, cellHeight, cellWidth * 3, cellHeight);
+    line(0, cellHeight * 2, cellWidth * 3, cellHeight * 2);
+
+    line(cellWidth, 0, cellWidth, cellHeight * 3);
+    line(cellWidth * 2, 0, cellWidth * 2, cellHeight * 3);
+}
+
+function makeMove() {
+    let x = floor(mouseX/cellWidth);
+    let y = floor(mouseY/cellHeight);
+
+    if(board[x][y] == '') {
+        board[x][y] = currentPlayer;
+        currentPlayer = ai;
+    }
+}
